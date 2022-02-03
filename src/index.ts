a03b253f88ece28034916576f0cb038d4df25e7d
Nate <mrtn.nathaniel@gmail.com>
2022-02-03 11:44:16 -0800

    index exports

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { createMatrix3Proxy, proxifyMatrix3 } from "./Matrix3";
+import { createMatrix4Proxy, proxifyMatrix4 } from "./Matrix4";
+import { createVector2Proxy, proxifyVector2 } from "./Vector2";
+import { createVector3Proxy, proxifyVector3 } from "./Vector3";
+import { createVector4Proxy, proxifyVector4 } from "./Vector4";
+
+export default {
+  createVector2Proxy,
+  proxifyVector2,
+  createVector3Proxy,
+  proxifyVector3,
+  createVector4Proxy,
+  proxifyVector4,
+  createMatrix3Proxy,
+  proxifyMatrix3,
+  createMatrix4Proxy,
+  proxifyMatrix4,
+}
