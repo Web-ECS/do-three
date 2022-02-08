57d5c637996082d2261f2ff51f3838654be9723d
Nate <mrtn.nathaniel@gmail.com>
2022-02-08 14:37:57 -0800

    typo fix

--- demo/createObject3DEntity.ts
+++ demo/createObject3DEntity.ts
@@ -1,7 +1,7 @@
 import { createObject3DProxy } from '../src/index'
 import * as THREE from 'three'
 import { Object3DProxy } from '../src/type/Object3D'
-import { object3DStore } from './Object3DStore'
+import { Object3DStore } from './Object3DStore'
 
 // start eid at 1, eid 0 acts as noop entity
 let eidCount = 1
@@ -10,7 +10,7 @@ export const createObject3DEntity = (
   geometry = new THREE.BoxGeometry(100,100,100),
   material = new THREE.MeshBasicMaterial({wireframe:true})
 ): Object3DProxy => {
-  const obj3d = createObject3DProxy(object3DStore, eidCount++)
+  const obj3d = createObject3DProxy(Object3DStore, eidCount++)
   const mesh = new THREE.Mesh(geometry,material)
   obj3d.add(mesh)
   return obj3d
